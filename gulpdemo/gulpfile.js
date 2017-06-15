cdf786452edd6bc3092a9ca7103ef77ad52fb148
junreturn1982 <ngochoang071982@gmail.com>
2017-06-15 14:26:55 +0700

    gulp-util fail

--- gulpdemo/gulpfile.js
+++ gulpdemo/gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const jshint = require('gulp-jshint');
 const jscs = require('gulp-jscs');
+const util = require('gulp-util');
 
 // gulp vet
 gulp.task('vet', () => {
+    log('Analyzing source with JSHint and JSCS');
     return gulp
             .src([
                 './src/**/*.js',
@@ -11,5 +13,19 @@ gulp.task('vet', () => {
             ])
             .pipe(jscs())
             .pipe(jshint())
-            .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
+            .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
+            .pipe(jshint.reporter('fail'));
 });
+
+//
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (let item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                util.log(util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        util.log(util.colors.blue(msg));
+    }
+}
